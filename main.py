f2506f4d3e56968ccfb29c879be572c27aea915b
Michael Lewis <Kenpachi@Mac.local>
2022-05-18 15:37:18 -0400

    stuff

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-import ccxtpro
-from asyncio import gather, run, sleep
 import os
-import aiofiles
-import zlib
-import base64
-import json
+import ccxtpro
+import lzma
 import time
-from datetime import datetime
+import logging
 
+from datetime import datetime
+from asyncio import gather, run
 from sms import send_sms
+
+FORMAT= '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT,level=logging.INFO)
+
+class OrderBook:
+    def __init__(self, object: dict):
+        self.bids: list = object.get('bids').copy()
+        self.asks: list = object.get('asks').copy()
+        self.timestamp: int = object.get('timestamp')
+
+    def format(self):
+
+        for i in range(0, len(self.bids)):
+            self.bids[i] = '{} {}'.format(self.bids[i][0], self.bids[i][1]) 
+
+        for i in range(0, len(self.asks)):
+            self.asks[i] = '{} {}'.format(self.asks[i][0], self.asks[i][1]) 
+
+        return 'timestamp:{};bids:{};asks:{}\n'.format(self.timestamp, ','.join(self.bids), ','.join(self.asks)).encode()
+
+class Trade:
+    def __init__(self, object: dict):
+        self.id: str = object.get('id')
+        self.price: float = object.get('price')
+        self.amount: float = object.get('amount')
+        self.side: str = object.get('side')
+        self.timestamp: int = object.get('timestamp')
+
+    def format(self):
+        return 'id:{};price:{};amount:{};side:{};timestamp:{}\n'.format(self.id, self.price, self.amount, self.side, self.timestamp).encode()
+
+def program_time():
+    try:
+        timestamp = int(time.time() * 1000)
+        utc = datetime.utcfromtimestamp(timestamp // 1000)
+        return utc.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-6] + "{:03d}".format(int(timestamp) % 1000) + 'Z'
+    except (TypeError, OverflowError, OSError):
+        return None
+
+def ymd(timestamp):
+    utc_datetime = datetime.utcfromtimestamp(int(round(timestamp / 1000)))
+    return utc_datetime.strftime('%Y-%m-%d')
 
 def directory_size(path):
 
@@ -25,120 +65,147 @@ async def size_monitor(path):
 
     while True:
 
-        await sleep(42300)
+        if time.time() % 86400 == 0:      
+            send_sms(f'Storage capacity at {directory_size(path) / (1000**3)}')
 
-        size = directory_size(path)
-        
-        send_sms(f'Storage capacity at {size / (1000**3)}g')
+def compress_data(filename):
+    try:
 
-def save_data(iso8601, path, method, data):
+        with open(filename, 'rb') as f:
+
+            compressed_file = '.'.join([filename, 'lzma'])
+            with lzma.open(compressed_file, 'wb') as lz:
+                lz.write(f.read())
+                lz.close()
+
+                logging.info('Saved {} {} bytes'.format(compressed_file, os.path.getsize(compressed_file)))
+
+            f.close()
+
+        os.remove(filename)
+
+    except Exception as e:
+        logging.error('compress data - {}'.format(str(e)))
+
+        send_sms('{}\n\nProblem compressing file'.format(program_time()))
+        raise e
+
+def save_data(path, data):
 
     try:
 
-        filename = '-'.join([method, iso8601])
-        with open(os.path.join(path, '.'.join([filename, 'zlib'])), mode='w') as f:
-            bytes = base64.b64encode(
-                zlib.compress(
-                    json.dumps(data).encode('utf-8')
-                )
-            ).decode('ascii')
-            f.write(bytes)
+        timestamp = data.timestamp
+        filename = os.path.join(path, ymd(timestamp))
+
+        # when date changes compress previous days data if it exists
+        if not os.path.exists(filename) and ymd(int(time.time() * 1000)) not in filename:
+
+            content = os.scandir(path)
+            for entry in content:
+                if entry.is_file() and 'lzma' not in entry.name:
+                    compress_data(entry.path)
+
+        with open(os.path.join(path, filename), 'ab') as f:
+            f.write(data.format())
+            f.close()
 
     except Exception as e:
-        print(e)
-        send_sms(f'Failed writing file')
+        logging.error('save data - {}'.format(str(e)))
+
+        send_sms('{}\n\nProblem saving file'.format(program_time()))
+        raise e
 
 async def symbol_loop(exchange, method, symbol, path):
 
-    trade_map: dict = {}
+    logging.info('Starting {} {} {}'.format(exchange.id, method, symbol))
 
-    print('Starting', exchange.id, method, symbol)
     while True:
 
         try:
             response = await getattr(exchange, method)(symbol)
             if method == 'watchOrderBook':
-                iso8601 = response.get('datetime')
-                save_data(iso8601, path, method, response)
+                save_data(path, OrderBook(response))
             elif method == 'watchTrades':
 
                 for item in response:
-
-                    if 'info' in item:
-                        del item['info']
-
-                    iso8601 = item.get('datetime')
-                    if iso8601 not in trade_map:
-
-                        for k, v in trade_map.items():
-
-                            v = sorted(v, key=lambda d: d['id'])
-                            save_data(iso8601, path, method, v)
-
-                        trade_map.clear()
-                        trade_map[iso8601] = [item]
-
-                        continue
-                    
-                    trade_map[iso8601].append(item)
-             
+                    save_data(path, Trade(item))
                 exchange.trades[symbol].clear()
 
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
 
-            print(e)
+            logging.error('symbol loop - {}'.format(str(e)))
 
-            # send_sms(f'{iso8601}\n\nSymbol loop error')
+            if type(e).__name__ == 'ExchangeError' or type(e).__name__ == 'Exception':
 
-            if type(e).__name__ == 'NetworkError':
-                raise ccxtpro.NetworkError(e)
+                send_sms('{}\n\nProblem watching {} {}'.format(program_time(), exchange.id, method))
+
+                raise e
+
+            await exchange.sleep(5000)
+            
 
 async def method_loop(exchange, method, symbols, path):
-    print('Starting', exchange.id, method, symbols)
+
+    directory = { 'watchOrderBook': 'order_book', 'watchTrades': 'trades' }
+
+    path = os.path.join(path, directory[method])
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     loops = [symbol_loop(exchange, method, symbol, path) for symbol in symbols]
     await gather(*loops)
 
 
-async def exchange_loop(exchange_id, methods, path):
+async def exchange_loop(exchange_id, methods, path, config = {}):
 
     path = os.path.join(path, exchange_id)
     if not os.path.exists(path):
         os.mkdir(path)
 
-    print('Starting', exchange_id, methods)
     exchange = getattr(ccxtpro, exchange_id)()
-    exchange.options.update({ 
-            'rateLimit': 10, 
-            'watchOrderBook': { 'depth': 'books' }
-        })
+    for attr, value in config.items():
+        if attr == 'options':
+            exchange.options.update(value)
+            continue
+
+        setattr(exchange, attr, value)
+
     loops = [method_loop(exchange, method, symbols, path) for method, symbols in methods.items()]
     await gather(*loops)
     await exchange.close()
 
 async def main():
 
-    save_path = os.path.join(os.getcwd(), '/mnt/volume_ams3_01/data/')
+    save_path = os.path.join(os.getcwd(), 'data/')
     if not os.path.exists(save_path):
         os.mkdir(save_path)
+
+    config = {
+        'okx': {
+            'options': { 
+                'rateLimit': 10, 
+                'watchOrderBook': { 'depth': 'books' }
+            }
+        }
+    }
 
     exchanges = {
         'okx': {
             'watchOrderBook': ['BTC/USDT:USDT'],
             'watchTrades': ['BTC/USDT:USDT'],
         },
-        # 'bitfinex': {
-        #     'watchOrderBook': ['BTC/USD'],
-        #     'watchTrades': ['BTC/USD'],
-        # },
+        'bitfinex': {
+            'watchOrderBook': ['BTC/USD'],
+            'watchTrades': ['BTC/USD'],
+        },
         'ftx': {
             'watchOrderBook': ['BTC/USD:USD'],
             'watchTrades': ['BTC/USD:USD'],
         },
     }
 
-    loops = [exchange_loop(exchange_id, methods, save_path) for exchange_id, methods in exchanges.items()]
+    loops = [exchange_loop(exchange_id, methods, save_path, config.get(exchange_id, {})) for exchange_id, methods in exchanges.items()]
     loops.append(size_monitor(save_path))
     await gather(*loops)
     
 run(main())
-
--- sms.py
+++ sms.py
@@ -1,8 +1,11 @@
 import logging
 import smtplib
+import time
 from email.mime.text import MIMEText
 
 logger = logging.getLogger(__name__)
+
+queue = []
 
 def exception(server: smtplib.SMTP, recipients: list, msg: str):
     msg = MIMEText(msg)
@@ -10,24 +13,51 @@ def exception(server: smtplib.SMTP, recipients: list, msg: str):
     msg['To'] = ', '.join(recipients)
 
     server.sendmail('botti.notification@gmail.com', recipients, msg.as_string())
+
+def delay_message(msg: str) -> bool:
+
+    delay = False
+
+    for i in range(0, len(queue)):
+
+        (sms, timestamp) = queue[i]
+        if msg == sms and int(time.time() * 1000) - timestamp < 3600:
+            delay = True
+
+    return delay
+
+def update_queue(msg: str) -> None:
     
+    for i in range(0, len(queue)):
+
+        (sms, timestamp) = queue[i]
+        if msg == sms:
+            queue.pop(i)
+            return
+
+    queue.append((msg, int(time.time() * 1000)))
+
 def send_sms(msg: str) -> None:
     try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
 
-            server.ehlo()
-            server.starttls()
-            server.login('botti.notification@gmail.com', 'yygakfowwmpogiuy')
-            
-            recipients = ['9286323030@vtext.com']
+        if not delay_message(msg):
 
-            msg = MIMEText(msg)
-            msg['From'] = 'botti.notification@gmail.com'
-            msg['To'] = ', '.join(recipients)
+            with smtplib.SMTP("smtp.gmail.com", 587) as server:
 
-            server.sendmail('botti.notification@gmail.com', recipients, msg.as_string())
+                server.ehlo()
+                server.starttls()
+                server.login('botti.notification@gmail.com', 'yygakfowwmpogiuy')
+                
+                recipients = ['9286323030@vtext.com']
 
-            server.close()
+                msg = MIMEText(msg)
+                msg['From'] = 'botti.notification@gmail.com'
+                msg['To'] = ', '.join(recipients)
+
+                server.sendmail('botti.notification@gmail.com', recipients, msg.as_string())
+                server.close()
+
+                update_queue(msg)
 
     except Exception as e:
         logger.error('failed sending sms {}'.format(str(e)))

f1907c28384ca12c5eb4f40aa6ee60bd435eb32b
Michael Lewis <Kenpachi@Mac.local>
2022-06-29 13:38:50 -0400

    updated server.sh

--- server.sh
+++ server.sh
@@ -2,10 +2,10 @@
 
 # start server
 if [ $1 = 'start' ]; then
-    nohup /usr/bin/python3 -u -m http.server --directory /mnt/volume_ams3_01/ > nohup.out &
+    nohup /usr/bin/python3 server.py -d /mnt/volume_ams3_01/ > nohup.out &
 fi
 
 # stop server
 if [ $1 = 'stop' ]; then
-    kill $(ps -ef | grep 'http.server' | awk '{print $2}' | head -1)
+    kill $(ps -ef | grep 'server.py' | awk '{print $2}' | head -1)
 fi
